fa2ee7d979dafa4af6b56283a9a02223f92d073e
ljkhyeong <jolri24@gmail.com>
2025-08-31 21:55:52 +0900

    test: rest-docs 가독성 리팩토링

--- src/test/java/com/myapp/ffs/api/FeatureFlagControllerTest.java
+++ src/test/java/com/myapp/ffs/api/FeatureFlagControllerTest.java
@@ -2,6 +2,8 @@ package com.myapp.ffs.api;
 
 import static org.mockito.BDDMockito.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -12,6 +14,8 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.payload.PayloadDocumentation;
+import org.springframework.restdocs.request.RequestDocumentation;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -32,6 +36,28 @@ public class FeatureFlagControllerTest {
 	FeatureFlagService featureFlagService;
 
 	@Test
+	void getFlag()	throws Exception {
+		given(featureFlagService.find(anyString(), anyString()))
+			.willReturn(new FeatureFlagResponseDto(1L, "checkout.newPayment", "stage", true));
+
+		mockMvc.perform(get("/api/flags/{env}/{key}", "stage", "checkout.newPayment")
+				.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andDo(document("get-flag",
+				pathParameters(
+					parameterWithName("env").description("환경 (e.g prod/stage)"),
+					parameterWithName("key").description("플래그 ")
+				),
+				responseFields(
+					fieldWithPath("id").description("플래그 ID").optional(),
+					fieldWithPath("flagKey").description("플래그 키"),
+					fieldWithPath("env").description("환경"),
+					fieldWithPath("enabled").description("활성화 여부")
+				)
+			));
+	}
+
+	@Test
 	void createFlag() throws Exception {
 		String json = """
             { "flagKey":"checkout.newPayment", "env":"stage", "enabled":true }
@@ -44,7 +70,13 @@ public class FeatureFlagControllerTest {
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(json))
 			.andExpect(status().isOk())
-			.andDo(document("create-flag"));
+			.andDo(document("create-flag",
+				responseFields(
+					fieldWithPath("id").description("플래그 ID").optional(),
+					fieldWithPath("flagKey").description("플래그 키"),
+					fieldWithPath("env").description("환경"),
+					fieldWithPath("enabled").description("활성화 여부")
+				)));
 	}
 
 	@Test
@@ -60,14 +92,22 @@ public class FeatureFlagControllerTest {
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(json))
 			.andExpect(status().isOk())
-			.andDo(document("update-flag"));
+			.andDo(document("update-flag",
+				pathParameters(
+					parameterWithName("id").description("플래그 ID")
+				)
+			));
 	}
 
 	@Test
 	void deleteFlag() throws Exception {
 		mockMvc.perform(delete("/api/flags/{id}", 1L))
 			.andExpect(status().isNoContent())
-			.andDo(document("delete-flag"));
+			.andDo(document("delete-flag",
+				pathParameters(
+					parameterWithName("id").description("플래그 ID")
+				)
+			));
 	}
 
 }
