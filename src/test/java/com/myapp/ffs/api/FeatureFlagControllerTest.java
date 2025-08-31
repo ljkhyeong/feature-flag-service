a8db53a706d020b10e08f4788806dce507348445
ljkhyeong <jolri24@gmail.com>
2025-08-31 23:43:16 +0900

    test: flag-get test 리팩토링
    성공, 실패 구분

--- src/test/java/com/myapp/ffs/api/FeatureFlagControllerTest.java
+++ src/test/java/com/myapp/ffs/api/FeatureFlagControllerTest.java
@@ -7,6 +7,10 @@ import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,14 +40,22 @@ public class FeatureFlagControllerTest {
 	FeatureFlagService featureFlagService;
 
 	@Test
-	void getFlag()	throws Exception {
-		given(featureFlagService.find(anyString(), anyString()))
-			.willReturn(new FeatureFlagResponseDto(1L, "checkout.newPayment", "stage", true));
+	@DisplayName("GET /api/flags/{env}/{key} - 200 성공")
+	void getFlag_success()	throws Exception {
+		// given
+		String key = "checkout.newPayment";
+		String env = "stage";
+		given(featureFlagService.find(key, env))
+			.willReturn(new FeatureFlagResponseDto(1L, key, env, true));
 
-		mockMvc.perform(get("/api/flags/{env}/{key}", "stage", "checkout.newPayment")
+		// when
+		// then
+		mockMvc.perform(get("/api/flags/{env}/{key}", env, key)
 				.accept(MediaType.APPLICATION_JSON))
 			.andExpect(status().isOk())
-			.andDo(document("get-flag",
+			.andExpect(jsonPath("$.flagKey").value(key))
+			.andExpect(jsonPath("$.env").value(env))
+			.andDo(document("get-flag-success",
 				pathParameters(
 					parameterWithName("env").description("환경 (e.g prod/stage)"),
 					parameterWithName("key").description("플래그 ")
@@ -56,6 +68,34 @@ public class FeatureFlagControllerTest {
 				)
 			));
 	}
+
+	@Test
+	@DisplayName("GET /api/flags/{env}/{key} - 200 성공")
+	void getFlag_fail() throws Exception {
+		// given
+		String key = "unknown";
+		String env = "stage";
+		given(featureFlagService.find(key, env))
+			.willThrow(new NoSuchElementException("flag not found"));
+
+		// when
+		// then
+		mockMvc.perform(get("/api/flags/{env}/{key}", env, key)
+				.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isNotFound())
+			.andDo(document("get-flag-404",
+				pathParameters(
+					parameterWithName("env").description("환경 (예: prod, stage)"),
+					parameterWithName("key").description("플래그 키")
+				),
+				responseFields(
+					fieldWithPath("code").description("에러 코드 (예: FLAG_NOT_FOUND)"),
+					fieldWithPath("message").description("에러 메시지"),
+					fieldWithPath("detail").description("에러 상세")
+					)
+			));
+	}
+
 
 	@Test
 	void createFlag() throws Exception {
