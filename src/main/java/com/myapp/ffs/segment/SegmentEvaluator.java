f613723e441bc8533639a29a616cb28a75e81097
ljkhyeong <jolri24@gmail.com>
2025-09-24 22:55:25 +0900

    fix: SegmentEvaluator null포인터 가능성 조치
    임시조치

--- src/main/java/com/myapp/ffs/segment/SegmentEvaluator.java
+++ src/main/java/com/myapp/ffs/segment/SegmentEvaluator.java
@@ -12,8 +12,8 @@ public class SegmentEvaluator {
 			boolean match = switch (condition.operator()) {
 				case EQUALS -> attributes.getOrDefault(condition.attribute(), "").equals(condition.value());
 				case NOT_EQUALS -> !attributes.getOrDefault(condition.attribute(), "").equals(condition.value());
-				case IN -> ((List<?>)condition.value()).contains(attributes.get(condition.attribute()));
-				case NOT_IN -> !((List<?>)condition.value()).contains(attributes.get(condition.attribute()));
+				case IN -> ((List<?>)condition.value()).contains(attributes.getOrDefault(condition.attribute(), "")); // condition.attribute가 널일수있음. 나중에 리팩토링하자
+				case NOT_IN -> !((List<?>)condition.value()).contains(attributes.getOrDefault(condition.attribute(), ""));
 				case MATCHES ->
 					attributes.getOrDefault(condition.attribute(), "").toString().matches(condition.value().toString());
 			};
