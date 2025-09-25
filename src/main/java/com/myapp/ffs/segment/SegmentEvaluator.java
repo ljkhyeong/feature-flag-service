d82d6984c89caceb9b650923bc43c4a6de985ce0
ljkhyeong <jolri24@gmail.com>
2025-09-25 22:52:55 +0900

    Refactor: Evaluator의 조건 처리 로직을 Strategy 패턴으로 리팩토링

--- src/main/java/com/myapp/ffs/segment/SegmentEvaluator.java
+++ src/main/java/com/myapp/ffs/segment/SegmentEvaluator.java
@@ -9,14 +9,7 @@ public class SegmentEvaluator {
 		boolean result = (logic == Logic.AND);
 
 		for (Condition condition : conditions) {
-			boolean match = switch (condition.operator()) {
-				case EQUALS -> attributes.getOrDefault(condition.attribute(), "").equals(condition.value());
-				case NOT_EQUALS -> !attributes.getOrDefault(condition.attribute(), "").equals(condition.value());
-				case IN -> ((List<?>)condition.value()).contains(attributes.getOrDefault(condition.attribute(), "")); // condition.attribute가 널일수있음. 나중에 리팩토링하자
-				case NOT_IN -> !((List<?>)condition.value()).contains(attributes.getOrDefault(condition.attribute(), ""));
-				case MATCHES ->
-					attributes.getOrDefault(condition.attribute(), "").toString().matches(condition.value().toString());
-			};
+			boolean match = OperatorStrategy.evaluate(condition, attributes);
 
 			if (logic == Logic.AND) {
 				result = result && match;
